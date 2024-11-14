d3ba7d1653283168ec614bc355b7f728f972b827
SERENA FERRARO <serenaferraro21@gmail.com>
2024-11-14 14:30:06 +0100

    API

--- script/script.js
+++ script/script.js
@@ -1,12 +1,28 @@
-const loadImgBtn = document.getElementById('load-image')
-const secondartImgBtn = document.getElementById('secondary-image')
-const hideBtn = document.getElementById('hide-btn')
+const loadImgBtn = document.getElementById("load-image");
+const secondartImgBtn = document.getElementById("secondary-image");
+const hideBtn = document.getElementById("hide-btn");
+const imgElements = document.querySelectorAll("img");
 
-
-
-const loadImgURL = 'https://api.pexels.com/v1/search?query=[your-query]'
-const secondartImgURL = 'https://api.pexels.com/v1/search?query=[your-secondary-query]'
+const loadImgURL = "https://api.pexels.com/v1/search?query=nature";
+const apiKey = 'n5BTjpmfhx2Ykmp0FhNNWxUSmAkAnVTJnYe9fMfubp2v4WTjYmmIPGNQ';
+const secondartImgURL = "https://api.pexels.com/v1/search?query=city";
 
 const loadImg = () => {
+  fetch(loadImgURL, {
+    headers: {
+      Authorization: apiKey
+    }
+  })
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Errore');
+    }
+  })
 
-}
+
+  .catch(error => console.error(error));
+};
+
+loadImgBtn.addEventListener("click", loadImg);
