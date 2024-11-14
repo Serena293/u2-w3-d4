c1116c79bf307d626d5e07672ac399d60994cca5
SERENA FERRARO <serenaferraro21@gmail.com>
2024-11-14 22:15:14 +0100

    ...

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ const secondaryImgBtn = document.getElementById("secondary-image");
 const imgElements = document.querySelectorAll("img");
 const btn = document.querySelectorAll("button");
 const hideBtn = document.getElementsByClassName("hide-btn");
-const cards = document.getElementsByClassName('cards')
+const cards = document.getElementsByClassName("cards");
 
 const loadImgURL = "https://api.pexels.com/v1/search?query=nature";
 const apiKey = "n5BTjpmfhx2Ykmp0FhNNWxUSmAkAnVTJnYe9fMfubp2v4WTjYmmIPGNQ";
@@ -57,33 +57,34 @@ const loadImg = (link) => {
 //     .catch((error) => console.error(error));
 // };
 
-
 const editToHide = () => {
   btn.forEach((button) => {
     if (button.innerText.trim() === "Edit") {
       button.innerText = "Hide";
       button.setAttribute("class", "hide-btn");
+      button.addEventListener('click', () => {
+        button.closest('.col-md-4').classList.add('d-none');
+      });
     }
   });
 };
 
-const removeCard = (i) => {    
-    cards[i].classList.add("d-none");
-  };
+// const removeCard = (i) => {
+//     cards[i].classList.add("d-none");
+//   };
 
 editToHide();
 
-loadImgBtn.addEventListener("click",()=>(loadImg(loadImgURL)));
-secondaryImgBtn.addEventListener("click", ()=>(loadImg(secondaryImgURL)));
+loadImgBtn.addEventListener("click", () => loadImg(loadImgURL));
+secondaryImgBtn.addEventListener("click", () => loadImg(secondaryImgURL));
 
-
-for (let i = 0; i < hideBtn.length; i++) {
-    hideBtn[i].addEventListener("click", ()=>{
-        for(let i=0; i<hideBtn.length; i++)
-            for(let j=0; j<cards.length; j++){
-            if(i===j){
-                cards[j].classList.add('d-none')
-            }
-        }
-    });
-  } //hideBtn racchiuse più elementi quindi è necessario un ciclo per aggiungere la funziona tutti i pulsanti
+// for (let i = 0; i < hideBtn.length; i++) {
+//     hideBtn[i].addEventListener("click", ()=>{
+//         for(let i=0; i<hideBtn.length; i++)
+//             for(let j=0; j<cards.length; j++){
+//             if(i===j){
+//                 cards[j].classList.add('d-none')
+//             }
+//         }
+//     });
+//   } //hideBtn racchiuse più elementi quindi è necessario un ciclo per aggiungere la funziona tutti i pulsanti
