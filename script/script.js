ac90c10c4e6ac68f97d8243d440adbc2d1d39e84
SERENA FERRARO <serenaferraro21@gmail.com>
2024-11-14 13:26:57 +0100

    set up js

---
+++ script/script.js
@@ -0,0 +1,12 @@
+const loadImgBtn = document.getElementById('load-image')
+const secondartImgBtn = document.getElementById('secondary-image')
+const hideBtn = document.getElementById('hide-btn')
+
+
+
+const loadImgURL = 'https://api.pexels.com/v1/search?query=[your-query]'
+const secondartImgURL = 'https://api.pexels.com/v1/search?query=[your-secondary-query]'
+
+const loadImg = () => {
+
+}
